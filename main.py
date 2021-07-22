7a3d24ee588b1ac081d8c1ebe9f5074ada1c5a58
Consecratio <jcasique@gmail.com>
2021-07-21 21:19:31 -0700

    end of lesson

---
+++ main.py
@@ -0,0 +1,50 @@
+# print("Hello world!")
+
+name = "Anna"
+address_num = 1234
+has_dog = True
+
+# print(name)
+# print(address_num)
+# print(has_dog)
+
+alphabet = "alfkmeafejfafebnagvbiez"
+
+# use if statement to find if alphabet has the letter 'z'
+# if statement ends with : and the following line must be tabbed or 4 spaces
+# if("z" in alphabet):
+#     print("Found a z")
+
+# example of a for loop
+# for i in range(1, 6):
+#     print(i)
+
+# arrays in python are called lists
+# example of lists
+foods = ["carrots", "kale", "beets"]
+
+# looping through a list
+# showing how to do interpolated strings
+# for food in foods:
+#     print("Hello, I currently have {} in the garden.".format(food))
+
+# example of function in python
+# def sum(num1, num2):
+#     return num1 + num2
+
+# total = sum(350, 50)
+# print(total)
+
+# example of built in list methods in py
+# colors = ["red", "yellow", "purple", "green"]
+# colors_length = len(colors)
+# print(colors_length)
+
+# colors.append("blue")
+# print(colors)
+
+# example of mathematical list operations
+batting_avgs = [.328, .348, .293, .293]
+sum_avgs = sum(batting_avgs)
+true_avgs = sum_avgs / len(batting_avgs)
+print(true_avgs)
